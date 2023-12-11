286a1e2a1d388bb8f8e6709fe5078ea0b5550dca
cikala1996 <cikaladubovac@gmail.com>
2023-12-11 19:25:14 +0100

    second commit

--- madlibs.py
+++ madlibs.py
@@ -8,5 +8,4 @@ famous=input("famous person")
 madlib=f"computer programming is so {adj}! it makes me so excited all time because\
 i love to {verb1}.Stay hydrated and {verb2} like you are {famous}"
 
-
-print(madlib)
+print(madlib,"partizan")
